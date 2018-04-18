ffa49340f64f506f1cca1c1270021c667f95d52b
pawnhearts <ph@kotchan.org>
2018-04-18 20:43:52 +0300

    fixeswq

--- qcon/qcon.py
+++ qcon/qcon.py
@@ -60,22 +60,34 @@ def find_term():
 
 class Process(object):
 
+    __instances__ = set()
+
     def __init__(self, name):
         self.name = name
         self.spawn_process()
-        Keybinder.bind(Config.get(self.name, 'Key'), self.on_key_press)
+        Keybinder.bind(conf.get(self.name, 'Key'), self.on_key_press)
 
     def spawn_process(self):
         os.chdir(os.path.expanduser('~'))
-        ps = Popen(shlex.split(Config.get(self.name, 'Command')), stdout=PIPE, stdin=PIPE)
+        ps = Popen(shlex.split(conf.get(self.name, 'Command')), stdout=PIPE, stdin=PIPE)
         self.pid = ps.pid
         GObject.child_watch_add(self.pid, self.on_child_exit)
         GObject.timeout_add(100, self._search_window)
 
     def _search_window(self):
+        scr = Wnck.Screen.get_default()
+        if conf.has_option(self.name, 'instance'):
+             for win in scr.get_windows():
+                 if win.get_class_instance_name() == conf.get(self.name, 'instance'):
+                     if conf.has_option(self.name, 'class'):
+                         if win.get_class_group_name() != conf.get(self.name, 'class'):
+                             continue
+                     self.window = win
+                     self.setup_window()
+                     return False
+
         if not is_process_running(self.pid):
             raise OSError('Child process for {} died'.format(self.name))
-        scr = Wnck.Screen.get_default()
         for win in scr.get_windows():
             if win.get_pid() == self.pid:
                 self.window = win
@@ -87,21 +99,21 @@ class Process(object):
         self.x11window = GdkX11.X11Window.foreign_new_for_display(GdkX11.X11Display.get_default(),
                                                                   self.window.get_xid())
         # func = Gdk.WMFunction.ALL
-        # for k, v in Config.defaults().items():
+        # for k, v in conf.defaults().items():
         #     if k.startswith('WM_'):
         #         func = func | getattr(Gdk.WMFunction,v[3:])
         # self.x11window.set_functions(func)
         # func = Gdk.WMDecoration.ALL
-        # for k, v in Config.defaults().items():
+        # for k, v in conf.defaults().items():
         #     if k.startswith('DEC_'):
         #         func = func | getattr(Gdk.WMDecoration,v[4:])
         # self.x11window.set_decorations(func)
 
-        self.x11window.set_opacity(Config.getfloat(self.name, 'Opacity'))
+        self.x11window.set_opacity(conf.getfloat(self.name, 'Opacity'))
         Wnck.Screen.get_default().connect('active_window_changed', self.on_hide_inactive)
         self.setup_geometry()
         self.window.stick()
-        if Config.getboolean(self.name, 'StartHidden'):
+        if conf.getboolean(self.name, 'StartHidden'):
             self.hide()
 
     def on_key_press(self, *args):
@@ -137,72 +149,75 @@ class Process(object):
             self.activate()
 
     def on_hide_inactive(self, screen, window):
-        if Config.getboolean(self.name, 'HideWhenLosesFocus'):
+        if conf.getboolean(self.name, 'HideWhenLosesFocus'):
             if window == self.window:
                 self.hide()
 
     def on_child_exit(self, pid, errcode):
         if errcode != 0:
             raise OSError('Terminal {} crashed'.format(self.name))
-        if Config.getboolean(self.name, 'Restart'):
+        if conf.getboolean(self.name, 'Restart'):
             Process(self.name)
             del self
         else:
             sys.exit()
 
+    def restart(self):
+        Process.__instances__.remove(self)
+        Process(self.name)
+        del self
+
     def setup_geometry(self):
-        if Config.getboolean(self.name, 'Decorations'):
+        if conf.getboolean(self.name, 'Decorations'):
             dec = Gdk.WMDecoration.TITLE | Gdk.WMDecoration.BORDER
             self.x11window.set_decorations(dec)
         else:
-            self.x11window.set_decorations(0)
+            self.x11window.set_decorations(Gdk.WMDecoration.BORDER)
 
-        if not Config.getboolean(self.name, 'Decorations'):
-            self.x11window.set_functions(Gdk.WMFunction.CLOSE | Gdk.WMFunction.MINIMIZE | Gdk.WMFunction.RESIZE)
         # BORDER, TITLE, MINIMIZE, MENU, RESIZEH, MAXIMIZE
 
-        fun = Gdk.WMFunction.CLOSE
-        functions = {'Movable': Gdk.WMFunction.MOVE,
-             'Minimizable': Gdk.WMFunction.MINIMIZE,
-             'Maximizable': Gdk.WMFunction.MAXIMIZE,
-             'Resizable': Gdk.WMFunction.RESIZE}
-        for k, v in functions.items():
-            if Config.get(self.name, k):
-                fun = fun | v
-        self.x11window.set_functions(fun)
+        # fun = Gdk.WMFunction.ALL
+        # functions = {'Movable': Gdk.WMFunction.MOVE,
+        #      'Minimizable': Gdk.WMFunction.MINIMIZE,
+        #      'Maximizable': Gdk.WMFunction.MAXIMIZE,
+        #      'Resizable': Gdk.WMFunction.RESIZE}
+        # for k, v in functions.items():
+        #     if  not conf.getboolean(self.name, k):
+        #         fun = fun | v
+        # self.x11window.set_functions(fun)
 
 
 
 
         # func = Gdk.WMFunction.ALL
-        # for k, v in Config.defaults().items():
+        # for k, v in conf.defaults().items():
         #     if k.startswith('WM_'):
         #         func = func | getattr(Gdk.WMFunction,v[3:])
         # self.x11window.set_functions(func)
         # func = Gdk.WMDecoration.ALL
-        # for k, v in Config.defaults().items():
+        # for k, v in conf.defaults().items():
         #     if k.startswith('DEC_'):
         #         func = func | getattr(Gdk.WMDecoration,v[4:])
         # self.x11window.set_decorations(func)
 
         scr = Wnck.Screen.get_default()
-        if Config.get(self.name, 'Width').endswith('%'):
-            w = scr.get_width() / 100.0 * int(Config.get(self.name, 'Width').rstrip('%'))
+        if conf.get(self.name, 'Width').endswith('%'):
+            w = scr.get_width() / 100.0 * int(conf.get(self.name, 'Width').rstrip('%'))
         else:
-            w = Config.getint(self.name, 'Width')
-        if Config.get(self.name, 'Height').endswith('%'):
-            h = scr.get_height() / 100.0 * int(Config.get(self.name, 'Height').rstrip('%'))
+            w = conf.getint(self.name, 'Width')
+        if conf.get(self.name, 'Height').endswith('%'):
+            h = scr.get_height() / 100.0 * int(conf.get(self.name, 'Height').rstrip('%'))
         else:
-            h = Config.getint(self.name, 'Height')
-        x = Config.getint(self.name, 'Offset-x')
-        if Config.get(self.name, 'Position-x').lower() == 'right':
+            h = conf.getint(self.name, 'Height')
+        x = conf.getint(self.name, 'Offset-x')
+        if conf.get(self.name, 'Position-x').lower() == 'right':
             x = scr.get_width() - x - w
-        if Config.get(self.name, 'Position-x').lower() == 'center':
+        if conf.get(self.name, 'Position-x').lower() == 'center':
             x = scr.get_width() / 2 - (w / 2) + x
-        y = Config.getint(self.name, 'Offset-y')
-        if Config.get(self.name, 'Position-y').lower() == 'bottom':
+        y = conf.getint(self.name, 'Offset-y')
+        if conf.get(self.name, 'Position-y').lower() == 'bottom':
             y = scr.get_height() - y -h
-        if Config.get(self.name, 'Position-y').lower() == 'center':
+        if conf.get(self.name, 'Position-y').lower() == 'center':
             y = scr.get_height() / 2 - (h / 2) + y
         self.x11window.move(x, y)
         self.x11window.resize(w, h)
@@ -211,6 +226,9 @@ class Process(object):
 
 def make_menu():
     menu = Gtk.Menu()
+    item_save = Gtk.MenuItem("Save window")
+    item_save.connect('activate', save_window)
+    menu.append(item_save)
     item_restart = Gtk.MenuItem("Restart")
     item_restart.connect('activate', restart)
     menu.append(item_restart)
@@ -230,11 +248,31 @@ def is_process_running(pid):
 
 
 def kill_children():
-    os.kill(os.getsid(os.getpid()), signal.SIGTERM)
+    os.kill(os.getsid(os.getpid()), signal.SIGKILL)
+    # for proc in Process.__instances__:
+    #     os.kill(proc.pid, signal.SIGKILL)
 
 
 def kill_other_copies():
     os.system(r"ps aux|grep qcon.py|grep -v %s|awk '{print $2}'|xargs kill -7" % os.getpid())
+
+def save_window(*args):
+    scr = Wnck.Screen.get_default()
+    win = scr.get_active_window()
+    x11window = GdkX11.X11Window.foreign_new_for_display(GdkX11.X11Display.get_default(),
+                                                                  win.get_xid())
+    section = win.get_class_group_name()
+    if not conf.has_section(section):
+        conf.add_section(section)
+    conf.set(section, 'Class', win.get_class_group_name())
+    conf.set(section, 'Instance', win.get_class_instance_name())
+    conf.set(section, 'Command', open('/proc/{}/cmdline'.format(win.get_pid())).read())
+    conf.set(section, 'Offset-x', str(x11window.get_position()[0]))
+    conf.set(section, 'Offset-y', str(x11window.get_position()[1]))
+    conf.set(section, 'Width', str(x11window.get_width()))
+    conf.set(section, 'Height', str(x11window.get_height()))
+    conf.write(open(os.path.expanduser("~/.qconrc"), 'w'))
+
 
 
 def restart():
@@ -252,9 +290,9 @@ ConfigDefaults = {
 'Position-y': 'top',
 'Offset-x': 0,
 'Offset-y': 0,
-'StartHidden': 0,
-'Restart': 0,
-'HideWhenLosesFocus': 0,
+'StartHidden': False,
+'Restart': True,
+'HideWhenLosesFocus': False,
 'Decorations': 1,
 'Opacity': 1.0,
 'Movable': 1,
@@ -262,8 +300,8 @@ ConfigDefaults = {
 'Minimizable': 1,
 'Maximizable': 1,
 }
-Config = configparser.ConfigParser(interpolation=None, defaults=ConfigDefaults)
-Config.read(os.path.expanduser('~/.qconrc'))
+conf = configparser.SafeConfigParser(defaults=ConfigDefaults, interpolation=None)
+conf.read(os.path.expanduser('~/.qconrc'))
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 signal.signal(signal.SIGHUP, restart)
 atexit.register(kill_children)
@@ -272,7 +310,7 @@ indicator = AppIndicator3.Indicator.new('qcon', 'qcon', AppIndicator3.IndicatorC
 indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
 indicator.set_menu(make_menu())
 indicator.set_icon('utilities-terminal')
-for section in Config.sections():
+for section in conf.sections():
     Process(section)
 Gtk.main()
 
